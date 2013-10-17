6d16c8d462fafb5d36e5b1ecbcd9d89e41ddaec6
don_philipe <don_philipe@gmx.de>
2013-10-17 10:03:30 +0200

    added some basic db-operations

--- src/DBConnector.py
+++ src/DBConnector.py
@@ -13,34 +13,71 @@ class DBConnector:
     #TODO: in-memory db necessary?
     def __init__(self, dbfile):
         """
-        Opens a connection to the database.
+        Opens a connection to the database, or creates
+        a new database if none exists.
         @param dbfile: the databasefile
         """
         self.__db = dbfile
         self.__conn = sqlite3.connect(self.__db)
+        self.__cursor = self.__conn.cursor()
+        try:
+            self.__cursor.execute("SELECT * FROM calendars")
+        except sqlite3.Error as e:
+            if "no such table" in e.args[0]:
+                print "missing tables, creating ..."
+                self.__createDB()
 
-    def newCalendar(self, name):
+    def __createDB(self):
         """
-        Adds a new calendar into the database.
-        @param name: the name of the calendar
+        Creates a new Database if none exists and adds the
+        tables neccessary.
         """
-        c = self.__conn.cursor()
         #TODO:
-        c.execute('''CREATE TABLE ?
-                             (date_start text, date_end text,
-                             comment text)''', name)
+        self.__cursor.execute('''CREATE TABLE calendars
+                             (cal_id text, cal_name text,
+                             description text)''')
+        self.__cursor.execute('''CREATE TABLE events
+                             (event_id text, cal_id text,
+                             event_start integer, event_end integer,
+                             title text, description text,
+                             alarm_id text)''')
+        self.__cursor.execute('''CREATE TABLE alarms
+                             (alarm_id text, start integer,
+                             last_ack integer)''')
+        self.__cursor.execute('''CREATE TABLE todos
+                             (todo_id text, cal_id text,
+                             title text, description text,
+                             alarm_id text)''')
         self.__conn.commit()
 
-    def delCalendar(self, name):
+    def newCalendar(self, cal_id, cal_name, cal_desc):
+        """
+        Adds a new calendar into the database.
+        @param cal_id:
+        @param cal_name: the name of the calendar
+        @param cal_desc:
+        """
+        #TODO:
+        self.__cursor.execute('''INSERT INTO calendars
+                             VALUES (?, ?, ?)''', cal_id, cal_name, cal_desc)
+        self.__conn.commit()
+
+    def delCalendar(self, cal_name):
         """
         Deletes a calendar from the database.
-        @param name: the name of the calendar
+        @param cal_name: the name of the calendar
         """
         c = self.__conn.cursor()
-        c.execute('''DROP TABLE ?''', name)
+        c.execute('''DELETE FROM calendars
+                WHERE cal_name=?''', cal_name)
         self.__conn.commit()
 
     def changeCalendar(self):
+        """
+        """
+        pass
+
+    def newEvent(self):
         """
         """
         pass
@@ -56,3 +93,9 @@ class DBConnector:
         Reopens the connection to the database.
         """
         self.__conn = sqlite3.connect(self.__db)
+
+    #TODO(later)
+    def checkDB(self):
+        """
+        """
+        pass
